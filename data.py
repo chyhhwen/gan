87888cd12a35cf372df25274bde604266583527a
chyhhwen <chyhhwen@gmail.com>
2023-02-12 01:02:36 +0800

    test

--- data.py
+++ data.py
@@ -6,3 +6,39 @@ from glob import glob
 import os.path as osp
 from PIL import Image
 from torchvision import transforms
+
+class Data(Dataset):
+    def __init__(self, folder=DATA_FOLDER, subset="train", transform=None):
+        img_paths = glob(osp.join(DATA_FOLDER, "*/*.jpg"))
+        train_paths, test_path = train_test_split(
+            img_paths, test_size = 0.2, random_state = 10
+        )
+        if subset == "train":
+            self.img_paths = train_paths
+        else:
+            self.img_paths = test_path
+        if transform is None:
+            self.transform = transforms.Compose(
+                [transforms.Resize((SIZE, SIZE)), transforms.ToTensor()]
+            )
+        else:
+            self.transform = transform
+
+    def __getitem__(self, index):
+        img = Image.open(self.img_paths[index]).convert("L")
+        img = self.transform(img)
+        return img, img
+    def __len__(self):
+        return len(self.img_paths)
+
+transform = transforms.Compose(
+    [
+        transforms.Resize((SIZE, SIZE)),
+        transforms.ToTensor()
+    ]
+)
+
+train_data = Data(subset = "train", transform = transform)
+val_data = Data(subset = "test")
+train_loader = DataLoader(train_data, batch_size = BATCH_SIZE, shuffle = True)
+val_loader = DataLoader(val_data, batch_size = BATCH_SIZE * 2, shuffle = True)
