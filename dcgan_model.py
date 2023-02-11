dc25d8f5e1a1d7b655d5bd60f98ff6018001b76c
chyhhwen <chyhhwen@gmail.com>
2023-02-11 18:49:09 +0800

    test

---
+++ dcgan_model.py
@@ -0,0 +1,7 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+def normal_init(m, mean, std):
+    if isinstance(m, nn.ConvTranspose2d) or isinstance(m, nn.Conv2d):
+        m.weight.data.normel_(mean, std)
